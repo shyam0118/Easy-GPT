0ee7b64eda426e168c6d11a738702bf973ebf5d3
Shyamalendu Nayak <shyamalendunayak0118@gmail.com>
2024-11-05 22:36:12 +0530

    local mongo db setup done

--- config/db.js
+++ config/db.js
@@ -3,9 +3,13 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb://localhost:27017/nexa-ai", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(
-      `Connected To Mongodb Database ${mongoose.connection.host}`.bgGreen.white
+      `Connected To Local Mongodb Database ${mongoose.connection.host}`.bgGreen
+        .white
     );
   } catch (error) {
     console.log(`Mongo Database Error ${error}`.bgRed.white);
